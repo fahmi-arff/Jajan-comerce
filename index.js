62ba516d82165619a6d0823da8b4f6198c27daa6
fahmi-arff <fahmi.arff@gmail.com>
2020-03-24 18:38:44 +0700

    add mongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const kategoris = require('./routes/kategoris');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/jajanOnlen', { useNewUrlParser: true, useUnifiedTopology: true  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.log('Could not connect ot MongoDB..'))
 
 app.use(express.json());
 app.use('/api/kategoris', kategoris);

4ca1e22c05b0df656b922c607b8aca9728195476
fahmi-arff <fahmi.arff@gmail.com>
2020-03-25 08:22:51 +0700

    add put,get id & delete on routes barang

--- routes/barangs.js
+++ routes/barangs.js
@@ -9,15 +9,23 @@ router.get('/', async (req, res) => {
   res.send(barang);
 });
 
+router.get('/:id', async (req, res) => {
+  const barang = await Barang.findById(req.params.id);
+
+  if (!barang) return res.status(404).send(`Barang dengan id ${req.params.id} tidak ditemukan`);
+
+  res.send(barang);
+});
+
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   const kategori = await Kategori.findById(req.body.kategoriId);
-  if (!kategori) return res.status(400).send('Kategori salah.');
+  if (!kategori) return res.status(400).send('Kategori ID salah.');
  
   const pengiriman = await Pengiriman.findById(req.body.pengirimanId);
-  if (!pengiriman) return res.status(400).send('pengiriman salah.');
+  if (!pengiriman) return res.status(400).send('pengiriman ID salah.');
 
   let barang = new Barang({ 
     nama: req.body.nama,
@@ -35,6 +43,46 @@ router.post('/', async (req, res) => {
     },
   });
   barang = await barang.save();
+
+  res.send(barang);
+});
+
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const kategori = await Kategori.findById(req.body.kategoriId);
+  if (!kategori) return res.status(400).send('Kategori salah.');
+ 
+  const pengiriman = await Pengiriman.findById(req.body.pengirimanId);
+  if (!pengiriman) return res.status(400).send('Pengiriman salah.');
+
+  const barang = await Barang.findByIdAndUpdate(req.params.id,
+    { 
+    nama: req.body.nama,
+    kategori: {
+      _id: kategori._id,
+      name: kategori.name
+    },
+    stok: req.body.stok,
+    harga: req.body.harga,
+    terjual: req.body.terjual,
+    pengiriman: {
+      _id: pengiriman._id,
+      kota: pengiriman.kota,
+      tarif: pengiriman.tarif
+    },
+    }, { new: true });
+
+  if (!barang) return res.status(404).send(`Barang dengan id ${req.params.id} tidak ditemukan`);
+
+  res.send(barang);
+});
+
+router.delete('/:id', async (req, res) => {
+  const barang = await Barang.findByIdAndRemove(req.params.id);
+
+  if (!barang) return res.status(404).send(`Barang dengan id ${req.params.id} tidak ditemukan`);
 
   res.send(barang);
 });

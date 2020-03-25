d7e9d3db3a33d6eef02044d658267e93a985ffbc
fahmi-arff <fahmi.arff@gmail.com>
2020-03-25 13:30:06 +0700

    authenticating users

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const kategoris = require('./routes/kategoris');
 const pengirimans = require('./routes/pengirimans');
 const barangs = require('./routes/barangs');
 const akuns = require('./routes/akuns');
+const login = require('./routes/login');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/jajanOnlen', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -17,6 +18,7 @@ app.use('/api/kategoris', kategoris);
 app.use('/api/pengirimans', pengirimans);
 app.use('/api/barangs', barangs);
 app.use('/api/akuns', akuns);
+app.use('/api/login',login);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} ...`));

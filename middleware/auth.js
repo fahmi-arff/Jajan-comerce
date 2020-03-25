e1dce82a62aed1be1e3baf80255afa6e34a594ee
fahmi-arff <fahmi.arff@gmail.com>
2020-03-25 15:33:36 +0700

    authorization middleware

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const { jwtPrivateKey } = require('../config');
+
+module.exports = function (req, res, next){
+  const token = req.header('x-auth-token');
+  if(!token) return res.status(401).send('Access denied. No token provided.');
+
+  try {
+    const deecoded = jwt.verify(token, jwtPrivateKey);
+    req.user = deecoded;
+    next();
+  } 
+  catch (ex) {
+    res.status(400).send('invalid token.')
+  }
+} 

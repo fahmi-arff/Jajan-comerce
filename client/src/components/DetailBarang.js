46d2ff03228d321d222a77e84325f40b260bce05
fahmi-arff <fahmi.arff@gmail.com>
2020-03-30 11:04:18 +0700

    show detail with conditional rendering

--- client/src/components/DetailBarang.js
+++ client/src/components/DetailBarang.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const DetailBarang = props => {
-  console.log(props);
-  return <div>DetailBarang</div>
+const DetailBarang = ({ barang }) => {
+  if(!barang){
+    return <div>Pilih Barang</div>
+  }
+  return (
+    <div>
+      <h3>Detail barang: </h3>
+      <p>
+        Nama Barang: {barang.nama}
+        <br />
+        Harga : {barang.harga}
+      </p>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {

49b0f5825f1174b5b550d293d77fca1e2fd4ae0e
fahmi-arff <fahmi.arff@gmail.com>
2020-04-01 19:02:49 +0700

    add header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route} from 'react-router-dom';
 import JList from './jajanOnlen/JList';
 import JCheckout from './jajanOnlen/JCheckout';
 import JDetail from './jajanOnlen/JDetail';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={JList} />
           <Route path="/jajan/detail" exact component={JDetail} />
           <Route path="/jajan/checkout" exact component={JCheckout} />

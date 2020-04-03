98601fed1a3cc6e4a09df3b4755eae27ac9fe96c
fahmi-arff <fahmi.arff@gmail.com>
2020-04-03 13:38:54 +0700

    customizing form fields

--- client/src/components/profileOnlen/CreateProfile.js
+++ client/src/components/profileOnlen/CreateProfile.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form'
 
 class CreateProfile extends React.Component {
-  renderInput({ input }){
-    return <input {...input}/>
+  renderInput({ input, label }){
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    )
   }
   render() {
     return (
-      <form>
-        <Field name="Nama Depan" component={this.renderInput} />
-        <Field name="Nama Belakang" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="Nama Depan" component={this.renderInput} label="Nama Depan" />
+        <Field name="Nama Belakang" component={this.renderInput} label="Nama Belakang" />
       </form>
     )
   }
